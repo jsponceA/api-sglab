311539bb47e27af8c9cf0999f7d24c0482660e31
jsponceA <jonny47169@gmail.com>
2024-01-16 15:35:17 -0500

    cambios pdf

--- resources/views/reportes/analisis/listado.blade.php
+++ resources/views/reportes/analisis/listado.blade.php
@@ -130,7 +130,7 @@
                                         <span style="margin: 0">{{$an->val_min.$an->separador.$an->val_max}}</span>
                                     @endif
                                     @if(!empty($an->referencia))
-                                        <span style="margin:0;font-size: 10px;">{!! nl2br($an->referencia) !!}</span>
+                                        <span style="margin:0;font-size: 12px;">{!! nl2br($an->referencia) !!}</span>
                                     @endif
                                 </td>
                             @endif
