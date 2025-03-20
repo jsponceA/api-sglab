791c4cc3efe384ea2a38cba226102a4a15c786e2
unknown <jonny47169@gmail.com>
2025-03-20 18:28:22 -0500

    corrigiendo reporte de analisis

--- resources/views/reportes/analisis/listado.blade.php
+++ resources/views/reportes/analisis/listado.blade.php
@@ -145,7 +145,7 @@
                                         <span style="margin: 0">{{$an->val_min.$an->separador.$an->val_max}}</span>
                                     @endif
                                     @if(!empty($an->referencia))
-                                        <span style="margin:0;font-size: 12px;">{!! nl2br(htmlspecialchars($an->referencia, ENT_QUOTES, 'UTF-8')) !!}</span>
+                                        <span style="margin:0;font-size: 11px;display: inline-block; line-height:1;">{!! nl2br(htmlspecialchars($an->referencia, ENT_QUOTES, 'UTF-8')) !!}</span>
                                     @endif
                                 </td>
                             @endif
